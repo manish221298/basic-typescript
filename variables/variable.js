e24bbed30404d5956a79176909adf75f858abc44
MKCH01 <iammnsh01@gmail.com>
2021-06-25 13:42:09 +0530

    basic-typescript

---
+++ variables/variable.js
@@ -0,0 +1,15 @@
+// Declaration of number variable in type script
+var num; // we can't assign other than number inthis variable
+num = 2;
+console.log("num", num);
+// e.g:- num = "manish" we can't assign
+var num1;
+num1 = 75.9;
+console.log("num1", num1);
+// Declaration of string variable in typescript
+var msg;
+msg = "manish kumar";
+console.log("msg", msg);
+var message;
+message = "chaurasia";
+console.log("my name is", msg + "  " + message);
---
+++ variables/variable.ts
@@ -0,0 +1,26 @@
+// Declaration of number variable in type script
+let num:number;  // we can't assign other than number inthis variable
+num = 2
+console.log("num", num)
+
+// e.g:- num = "manish" we can't assign
+
+let num1 : number
+num1 = 75.9
+console.log("num1", num1)
+
+// Declaration of string variable in typescript
+
+let msg : string
+msg = "manish kumar"
+console.log("msg", msg)
+
+let message : string
+
+message = "chaurasia"
+
+console.log("my name is", `${msg}  ${message}`)
+
+// Declaration of Boolean variable in typescript
+
+
